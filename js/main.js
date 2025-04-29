e78bfc61d12c81f5f8f724193918303e1322b97e
Alissa-MG <alissagarcia10118@gmail.com>
2025-04-29 17:03:51 -0500

    download-btn-popup

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const downloadButton = document.querySelector('#download-btn');
+
+downloadButton.addEventListener('click', function(){
+    alert('Thanks! Your itinerary will be downloaded soon.');
+});
