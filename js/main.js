932f62b3bfd79b00a29558dcb9d7eb71ee32a994
Alissa-MG <alissagarcia10118@gmail.com>
2025-04-29 19:15:17 -0500

    Refactor download button and add popup functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-const downloadButton = document.querySelector('#download-btn');
+//downloadButton.addEventListener('click', function () {
+//   alert('Thanks! Your itinerary will be downloaded soon.');
+//});
 
-downloadButton.addEventListener('click', function(){
-    alert('Thanks! Your itinerary will be downloaded soon.');
+window.addEventListener('DOMContentLoaded', function () {
+    const downloadButton = this.document.querySelector('#download-btn');
+    const popup = document.querySelector('#popup');
+    const closePopup = document.querySelector('#close-popup');
+   
+    if (!downloadButton || !popup || !closePopup) {
+        console.log('One or more elements are missing!');
+        return;
+    }
+        downloadButton.addEventListener('click', function () {
+            popup.classList.remove('hidden');
+        });
+        closePopup.addEventListener('click', function() {
+            popup.classList.add('hidden');
+        });
 });
+
+
+
